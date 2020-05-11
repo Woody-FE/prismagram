5a82b54ae0e47e8a7c43a451b7d29fafae731920
Taein Kim <kti91@likelion.org>
2020-05-12 00:19:10 +0900

    200512 | added computed resolvers

---
+++ src/api/User/computed.js
@@ -0,0 +1,28 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+    User: {
+        fullName: (parent) => {
+            return `${parent.lastName} ${parent.firstName}`
+
+        },
+        isFollowing: (parent, __, { request }) => {
+            const { user } = request;
+            const { id: parentId } = parent;
+            try {
+                return prisma.$exists.user({
+                    AND: [
+                        { id: user.id }, { followings_some: { id: parentId } }
+                    ]
+                })
+            } catch {
+                return false;
+            }
+        },
+        isSelf: (parent, __, { request }) => {
+            const { user } = request;
+            const { id: parentId } = parent;
+            return user.id === parentId
+        }
+    }
+}
